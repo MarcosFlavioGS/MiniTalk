76a3f53e93fcfb666fc4c1fd68620864150dce6c
MarcosFlavioGS <mfghost69@gmail.com>
2023-01-24 17:56:20 -0300

    Mandando mensagens

--- client.c
+++ client.c
@@ -6,13 +6,45 @@
 /*   By: mflavio- <mflavio-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 16:03:44 by mflavio-          #+#    #+#             */
-/*   Updated: 2023/01/24 16:03:47 by mflavio-         ###   ########.fr       */
+/*   Updated: 2023/01/24 17:54:20 by mflavio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int main(void)
+void    ft_send_bits(int pid, char c)
 {
+    int i;
 
+    i = 0;
+    while (i < 8)
+    {
+        if (c & (1 << i))
+            kill(pid, SIGUSR1);
+        else
+            kill(pid, SIGUSR2);
+        i++;
+        usleep(100);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    int pid;
+    int i;
+
+    if (argc != 3)
+    {
+        ft_printf("Usage: ./client [server PID] [message]");
+        return (0);
+    }
+    pid = ft_atoi(argv[1]);
+    i = 0;
+    while (argv[2][i])
+    {
+        ft_send_bits(pid, argv[2][i]);
+        i++;
+    }
+    ft_send_bits(pid, '\n');
+    return (0);
 }
--- server.c
+++ server.c
@@ -6,17 +6,39 @@
 /*   By: mflavio- <mflavio-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 16:04:21 by mflavio-          #+#    #+#             */
-/*   Updated: 2023/01/24 16:04:23 by mflavio-         ###   ########.fr       */
+/*   Updated: 2023/01/24 17:39:52 by mflavio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void handler(int sig)
+{
+   static int	bit;
+	static int	i;
+
+	if (sig == SIGUSR1)
+		i |= (1 << bit);
+	bit++;
+	if (bit == 8)
+	{
+		ft_printf("%c", i);
+		bit = 0;
+		i = 0;
+	} 
+}
+
 int main(void)
 {
-	int pid;
-	
-	pid = getpid();
-	ft_printf("PID: %d", pid);
+    int pid;
+
+    pid = getpid();
+    ft_printf("Server PID: %d\n", pid);
+    while (1)
+	{
+		signal(SIGUSR1, handler);
+		signal(SIGUSR2, handler);
+        pause();
+	}
 	return (0);
 }
