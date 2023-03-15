41e008508214315f9154cb7b93564549ce9559a0
MarcosFlavioGS <mfghost69@gmail.com>
2023-03-14 22:19:04 -0300

    Realocando o i++ e diminuindo o usleep

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: mflavio- <mflavio-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 16:03:44 by mflavio-          #+#    #+#             */
-/*   Updated: 2023/01/24 18:43:33 by mflavio-         ###   ########.fr       */
+/*   Updated: 2023/03/14 22:12:59 by mflavio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,11 @@ void	ft_send_bits(int pid, char c)
 	i = 0;
 	while (i < 8)
 	{
-		if (c & (1 << i))
+		if (c & (1 << i++))
 			kill(pid, SIGUSR1);
 		else
 			kill(pid, SIGUSR2);
-		i++;
-		usleep(500);
+		usleep(300);
 	}
 }
 
